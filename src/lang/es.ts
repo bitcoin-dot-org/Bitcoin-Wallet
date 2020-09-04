01242bbf52bf7a5d3875b9ddde08229222263b10
V. Sus <55538293+v-sus@users.noreply.github.com>
2020-09-04 03:01:56 +0200

    Some corrections (es.ts)

--- src/lang/es.ts
+++ src/lang/es.ts
@@ -7,20 +7,20 @@ import Language from '@/lang/langInterface'
 var es = <Language> {
     // Initial page
     getting_started: 'Empezando',
-    create_new: 'Crear nueva billetera',
+    create_new: 'Crear nuevo monedero',
     to_start: 'para empezar a usar Bitcoin',
     or: 'o',
-    restore_existing: 'restaurar billetera existente',
+    restore_existing: 'restaurar monedero existente',
     something_wrong: 'Algo salió mal con la conexión',
     try_again: 'Inténtalo de nuevo',
 
     // Create page
-    generate_notice: "Generaremos para usted una frase inicial de 12 palabras. Es muy importante que anote cada palabra en el orden correcto y la guarde de forma segura.",
-    wrote_it_down: 'Lo escribí',
+    generate_notice: "Generaremos una semilla de 12 palabras para ti. Es muy importante que anotes cada palabra en el orden correcto y la guardes de forma segura.",
+    wrote_it_down: 'La he guardado',
     generate: 'Generar',
 
     // Restore page
-    restore_notice: 'Ingrese su frase inicial de 12 palabras para restaurar su billetera',
+    restore_notice: 'Ingresa la semilla de 12 palabras para restaurar tu monedero',
 
     // Wallet home
     refresh: 'Actualizar',
@@ -30,51 +30,51 @@ var es = <Language> {
     settings: 'Configuraciones',
     amount_to_send: 'Cantidad a enviar',
     not_enough_balance: 'No hay saldo suficiente para enviar esa cantidad',
-    send_max: 'Enviar máximo',
+    send_max: 'Enviar el máximo',
     amount: 'Cantidad',
-    miner_fee: 'Tarifa de minero',
+    miner_fee: 'Comisión',
     total: 'Total',
     confirmation: 'Confirmación',
-    sending: 'Estas enviando',
+    sending: 'Estás enviando',
     to: 'a',
-    im_sure: "Estoy segura enviar",
-    seed_modal: 'Frase semilla',
+    im_sure: "Estoy seguro, enviar",
+    seed_modal: 'Semilla',
 
     // Overview
-    no_transactions: "Lamentablemente, aún no tiene ninguna transacción",
+    no_transactions: "Lamentablemente, aún no tienes ninguna transacción...",
     block_height: 'Altura del bloque',
     status: 'Estado',
     processing: 'Procesando',
-    unconfirmed: 'Inconfirmado',
-    complete: 'Completo',
+    unconfirmed: 'Sin confirmar',
+    complete: 'Completado',
 
     // Send
-    address: 'Habla a',
-    enter_address: 'Ingrese la dirección a enviar también y el monto:',
-    low_priority: 'Baja prioridad',
+    address: 'Dirección',
+    enter_address: 'Ingresa la dirección y la cantidad:',
+    low_priority: 'Prioridad baja',
     standard: 'Estándar',
     important: 'Importante',
-    low_priority_desc: "Paga menos tarifas, pero considera que esta es una transacción de baja prioridad y no le importa esperar más tiempo para la confirmación.",
-    standard_desc: "Esto usará una tarifa moderada, en momentos de gran congestión, esto puede llevar a un tiempo de confirmación más largo, pero estos son raros.",
-    important_desc: "Utiliza una tarifa premium para orientar la confirmación dentro de los 30 minutos, esta es una transacción de alta prioridad y desea que se confirme rápidamente.",
-    not_enough: "No tiene suficiente para enviar la cantidad especificada y al mismo tiempo paga la tarifa del minero. Prueba 'Enviar Max'.",
-    dust_error: "Cantidad demasiado pequeña para enviar",
+    low_priority_desc: "Pagas menos comisiones pero considerando que esta es una transacción de baja prioridad y no te importa esperar más tiempo para su confirmación.",
+    standard_desc: "Se usará una comisión moderada aunque en momentos de gran congestión esto puede comportar un tiempo de confirmación más largo (ocurre pocas veces)",
+    important_desc: "Se usará una comisión premium para asegurar la confirmación en menos de 30 minutos, esta es una transacción de alta prioridad para cuando se desea una confirmación rápida.",
+    not_enough: "No tienes suficiente para enviar la cantidad especificada y cubrir la tarifa de minado. Prueba con la opción 'Enviar el máximo'.",
+    dust_error: "Cantidad demasiado pequeña para el envío...",
 
     // Receive
-    receive_only: 'Reciba solo Bitcoin',
+    receive_only: 'Recibir solo Bitcoin',
     address_below: 'a la siguiente dirección',
 
     // Settings
-    show_seed: 'Mostrar frase inicial',
+    show_seed: 'Mostrar semilla',
     language: 'Idioma',
     currency: 'Moneda',
-    exit_wallet: 'Salir de billetera',
+    exit_wallet: 'Salir del monedero',
 
     // Buttons
-    back_button: 'Espalda',
+    back_button: 'Atrás',
     restore_button: 'Restaurar',
-    next_button: 'Próximo',
-    ok_button: 'Bueno',
+    next_button: 'Siguiente',
+    ok_button: 'OK',
     copy_button: 'Copiar'
 }
 
