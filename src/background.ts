c71cb9a4decb7cd27f5f76956c458a0eb8a10a89
alexcherman <sashacherman@gmail.com>
2020-10-22 14:00:29 +0200

    Remove the ability to resize the screen

--- src/background.ts
+++ src/background.ts
@@ -18,7 +18,7 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+  win = new BrowserWindow({ width: 800, height: 600, resizable: false, webPreferences: {
     // Use pluginOptions.nodeIntegration, leave this alone
     // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
     nodeIntegration: false
