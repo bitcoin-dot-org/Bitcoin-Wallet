f1c95d3783a2ab80a048a354cb7d0297b7409f50
Vicent Sus <55538293+vsus@users.noreply.github.com>
2020-11-25 00:32:03 +0100

    Update ca.ts

--- src/lang/ca.ts
+++ src/lang/ca.ts
@@ -6,38 +6,38 @@ import Language from '@/lang/langInterface'
 
 var ca = <Language> {
     // Initial page
-    getting_started: 'Començar',
-    create_new: 'Generar nou moneder',
-    create_subtext: "We'll create a new wallet for you so you can start using Bitcoin.",
+    getting_started: 'Començant',
+    create_new: 'Crear un nou moneder',
+    create_subtext: "Crearem un nou moneder perquè puguis començar a utilitzar Bitcoin.",
     restore_existing: 'restaurar moneder existent',
-    restore_subtext: "You already have a wallet and wish to use your recovery phrase to restore it.",
-    change_language: 'Change language',
-    choose_language: 'Choose Language',
+    restore_subtext: "Ja tens un moneder i vols utilitzar les paraules de seguretat per restaurar-lo.",
+    change_language: 'Canviar idioma',
+    choose_language: 'Escollir idioma',
 
     // Create page
-    seed_phrase: "Recovery Words",
-    wrote_it_down: 'Ho he guardat',
+    seed_phrase: "Llavor (paraules de seguretat)",
+    wrote_it_down: "L’he guardat",
     generate: 'Generar',
-    warning: 'Warning',
-    we_will_generate: 'We will generate for you 12 recovery words.',
-    warning_text_1: "Write the words down in the correct order, and keep them safe, offline. If you lose these words, you won't be able to access your Bitcoin.",
-    warning_text_2: "These recovery words will allow you to recover your wallet. Write the words down and keep them safe, offline.",
-    write_it_down: 'Write it down',
-    keep_it_safe: 'Keep it safe',
-    do_not_lose_it: 'Don’t lose it',
-    have_saved: 'I have saved my recovery words safely.',
+    warning: 'Advertència',
+    we_will_generate: 'Es generaran 12 paraules de seguretat.',
+    warning_text_1: "Escriu les paraules en l'ordre correcte i guarda-les en un lloc segur (offline). Si perds aquestes paraules, no podrás accedir als teus bitcoins.",
+    warning_text_2: "Aquestes paraules de seguretat et permetran recuperar el moneder. Escriu-les en un lloc segur (offline).",
+    write_it_down: 'Anota la llavor',
+    keep_it_safe: 'Guarda-la en un lloc segur',
+    do_not_lose_it: 'No la perdis',
+    have_saved: 'He guardat les paraules de seguretat en un lloc segur.',
 
     // Restore page
-    restore_notice: 'Please enter your recovery words to restore your wallet.',
-    restore_warning: 'The recovery words you have entered are invalid. Words must be spelled correctly and have no capital letters.',
-    restoring: 'Restoring...',
+    restore_notice: 'Escriu les paraules de seguretat per restaurar el teu moneder.',
+    restore_warning: "Les paraules de seguretat que has introduit no són vàlides. Les paraules han d'estar escrites correctament i no han de contenir majúscules.",
+    restoring: 'Restaurant...',
 
     // Wallet home
     refresh: 'Actualitzar',
     overview: 'Resum',
     send: 'Enviar',
     receive: 'Rebre',
-    settings: 'Configuracions',
+    settings: 'Configuració',
     amount_to_send: 'Quantitat a enviar',
     not_enough_balance: 'No hi ha suficients fons per enviar aquesta quantitat',
     send_max: 'Enviar el màxim',
@@ -45,31 +45,31 @@ var ca = <Language> {
     miner_fee: 'Comissió',
     total: 'Total',
     confirmation: 'Confirmació',
-    sending: 'Sending...',
+    sending: 'Enviant...',
     im_sure: "Estic segur, enviar",
     seed_modal: 'Recovery Words',
-    transaction: 'transaction',
-    transactions: 'transactions',
-    buy_bitcoin: 'Buy Bitcoin',
+    transaction: 'transacció',
+    transactions: 'transaccions',
+    buy_bitcoin: 'Comprar Bitcoin',
 
     // Pagination
-    page: 'Page', // Displayed as 'Page 1 of 1'
-    of: 'of',
+    page: 'Pàgina', // Displayed as 'Page 1 of 1'
+    of: 'de',
 
     // Overview
 
-    total_balance: "Total Balance",
-    no_transactions: "You have not made any transactions yet",
-    what_to_do: "What would you like to do?",
-    date: 'Date',
+    total_balance: "Fons",
+    no_transactions: "Encara no has fet cap transacció",
+    what_to_do: "Què t'agradaria fer?",
+    date: 'Data',
     status: 'Estat',
     processing: 'Processant',
     unconfirmed: 'Sense confirmar',
     complete: 'Completat',
 
     // Send
-    send_to: 'Send only to Bitcoin (BTC) addresses',
-    bitcoin_address: 'Bitcoin address',
+    send_to: 'Enviar només a direccions de Bitcoin (BTC)',
+    bitcoin_address: 'Adreça de Bitcoin',
     address: 'Adreça',
     low_priority: 'Prioritat baixa',
     standard: 'Estàndard',
@@ -77,17 +77,17 @@ var ca = <Language> {
     low_priority_desc: "Pagues menys comissions però considerant que aquesta transacció és de baixa prioritat i no et fa res esperar més temps a la confirmació.",
     standard_desc: "S'utilitzarà una comissió moderada tot i que en moments de gran congestió això pot comportar un temps de confirmació més llarg (passa poques vegades)",
     important_desc: "S'utilitzarà una comissió premium per assegurar una confirmació en menys de 30 minuts, aquesta és una transacció de prioritat alta per quan es necessita una confirmació ràpida.",
-    not_enough: "You don't have enough to send that amount and also pay the network fee. Try sending your entire balance with the Send Max button instead.",
-    dust_error: "The amount you are trying to send is too small for the Bitcoin network to process.",
-    bitcoin_network_fee: "Bitcoin Network fee",
+    not_enough: "No tens suficients fons per enviar la quantitat especificada i pagar la comissió dels miners. Prova amb l'opció 'Enviar el màxim'.",
+    dust_error: "La quantitat que estàs intentant enviar és massa petita perquè la xarxa Bitcoin la pugui processar.",
+    bitcoin_network_fee: "Comissió de xarxa",
 
     // Receive
     receive_only: 'Rebre només Bitcoin',
-    wallet_address: 'Wallet address',
+    wallet_address: 'Adreça del moneder',
     address_below: 'a la següent adreça',
 
     // Settings
-    show_seed: 'Recovery',
+    show_seed: 'Recuperar',
     language: 'Idioma',
     currency: 'Moneda',
     exit_wallet: 'Sortir del moneder',
@@ -98,16 +98,16 @@ var ca = <Language> {
     next_button: 'Següent',
     ok_button: 'OK',
     copy_button: 'Copiar',
-    save_button: 'Save',
+    save_button: 'Guardar',
 
     // Notification
-    notification_title: 'Incoming Transaction',
+    notification_title: 'Transacció Entrant',
 
     // Exit wallet
-    exit_text: 'This will destroy your wallet files and all records of your transactions. The only way to restore your wallet will be through your recovery words',
-    exit_are_you_sure: 'Are you sure you want to delete and exit this wallet?',
-    exit_label_text: 'Yes, delete this wallet',
-    exit_delete: 'Delete',
+    exit_text: "Això destruirà els arxius del moneder i tots els registres de les teves transaccions. L'única forma de restaurar el teu moneder serà a través de les paraules de seguretat.",
+    exit_are_you_sure: "Estàs segur que vols eliminar i sortir d'aquest moneder?",
+    exit_label_text: 'Sí, eliminar aquest moneder',
+    exit_delete: 'Eliminar',
 }
 
 export default ca
