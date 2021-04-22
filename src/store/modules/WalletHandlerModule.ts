88a0cd82cbd669f5174d8b98390d791edd98ef20
Furkan Atesli <31884209+furkanatesli@users.noreply.github.com>
2021-04-22 15:54:19 +0300

    Update WalletHandlerModule.ts

--- src/store/modules/WalletHandlerModule.ts
+++ src/store/modules/WalletHandlerModule.ts
@@ -8,6 +8,7 @@ import ja from '@/lang/ja'
 import ca from '@/lang/ca'
 import pt_br from '@/lang/pt_br'
 import it from '@/lang/it'
+import tr from '@/lang/tr'
 import store from '@/store'
 import BigNumber from 'bignumber.js'
 
